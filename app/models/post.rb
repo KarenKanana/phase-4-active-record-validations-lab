a38a72f18a7477929a1855d8f0fef73dc188d7e8
Karen Kanana <kananakaren21@gmail.com>
2022-12-02 15:53:57 +0300

    Complete assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates :title, exclusion: {in: ['True Facts']}
+
 end
+# def is_clickbait?
+#     if !title.nil? && !title.include?("Won
+#         t Believe" || "Secret" || "Top
+#         [number]" || "Guess")
+#         errors.add(:title, "must be clickbait")
+#     end
+# end
+
+
